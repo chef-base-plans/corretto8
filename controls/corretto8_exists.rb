77f29636c89ee7d0f25bc218a314ba0d64787efa
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-09-07 17:59:34 +0100

    Fix issue caused because plan.sh was failing to patchelf all the binaries correctly.  Also (1) add test suite to verify the fix; and (2) add the jre binaries as well to the full suite
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/corretto8_exists.rb
+++ controls/corretto8_exists.rb
@@ -7,9 +7,11 @@ control 'core-plans-corretto8-exists' do
   impact 1.0
   title 'Ensure corretto8 exists'
   desc '
-  Verify corretto8 by ensuring bin/java 
-  (1) exists and
-  (2) is executable'
+  Verify corretto8 by ensuring
+  (1) its installation directory exists
+  (2) its jdk binaries are all executable
+  (3) its jre binaries are all executable
+  '
   
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
   describe plan_installation_directory do
@@ -68,5 +70,25 @@ control 'core-plans-corretto8-exists' do
         it { should exist }
         it { should be_executable }
       end
-    end
+  end
+    
+  [
+    "java",
+    "jjs",
+    "keytool",
+    "orbd",
+    "pack200",
+    "policytool",
+    "rmid",
+    "rmiregistry",
+    "servertool",
+    "tnameserv",
+    "unpack200",
+  ].each do |binary_name|
+      command_full_path = File.join(plan_installation_directory.stdout.strip, "jre", "bin", binary_name)
+      describe file(command_full_path) do
+        it { should exist }
+        it { should be_executable }
+      end
+  end
 end
--- controls/corretto8_works.rb
+++ controls/corretto8_works.rb
@@ -175,4 +175,63 @@ control 'core-plans-corretto8-works' do
       its(io) { should match command_output_pattern }
     end
   end
+
+  {
+    "java" => {
+      io: "stderr",
+    },
+    "jjs" => {
+      io: "stderr",
+      command_output_pattern: /jjs \[<options>\] <files> \[-- <arguments>\]/,
+      exit_pattern: /^[^0]{1}\d*$/,
+    },
+    "keytool" => {
+      io: "stderr",
+      command_output_pattern: /Key and Certificate Management Tool/,
+    },
+    # ignoring orbd because this starts a daemon
+    # "orbd" => {},
+    "pack200" => {
+      exit_pattern: /^[^0]{1}\d*$/,
+    },
+    "policytool" => {
+      exit_pattern: /^[^0]{1}\d*$/,
+    },
+    "rmid" => {
+      io: "stderr",
+      exit_pattern: /^[^0]{1}\d*$/,
+    },
+    "rmiregistry" => {
+      io: "stderr",
+      exit_pattern: /^[^0]{1}\d*$/,
+    },
+    "servertool" => {
+      command_prefix: "echo 'quit' | ",
+      command_suffix: "",
+      command_output_pattern: /Welcome to the Java IDL Server Tool/,
+    },
+    # start server, verify output, kill after 1 second timeout
+    "tnameserv" => {
+      command_prefix: "timeout 1",
+      exit_pattern: /^[^0]{1}\d*$/,
+      command_output_pattern: /TransientNameServer: setting port for initial object references to: 900/,
+    },
+    "unpack200" => {
+      exit_pattern: /^[^0]{1}\d*$/,
+    },
+  }.each do |binary_name, value|
+    # set default values if each binary doesn't define an over-ride
+    command_prefix = value[:command_prefix] || ""
+    command_suffix = value[:command_suffix] || "-help"
+    command_output_pattern = value[:command_output_pattern] || /usage:.+#{binary_name}/i 
+    exit_pattern = value[:exit_pattern] || /^[0]$/ # use /^[^0]{1}\d*$/ for non-zero exit status
+    io = value[:io] || "stdout"
+  
+    # verify output
+    command_full_path = File.join(plan_installation_directory.stdout.strip, "jre", "bin", binary_name)
+    describe bash("#{command_prefix} #{command_full_path} #{command_suffix}") do
+      its('exit_status') { should cmp exit_pattern }
+      its(io) { should match command_output_pattern }
+    end
+  end
 end
